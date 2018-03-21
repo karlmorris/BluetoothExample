c11d7ba1a867a91f7fd9d88b78860669057d545a
karl <karl@macc.io>
2018-03-20 20:42:09 -0400

    Listening for enabled bluetooth

--- app/src/main/java/com/example/tuf80213/bluetoothexample/MainActivity.java
+++ app/src/main/java/com/example/tuf80213/bluetoothexample/MainActivity.java
@@ -3,41 +3,79 @@ package com.example.tuf80213.bluetoothexample;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends Activity {
+
+    ListView deviceListView;
+    BluetoothAdapter bta;
+
+    private final int REQUEST_BLUETOOTH = 111;
+
+
+    // Enable bluetooth functionality in app when Bluetooth enabled on device
+    BroadcastReceiver bluetoothEnabledReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(intent.getAction()))
+                if (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF) == BluetoothAdapter.STATE_ON)
+                    setupBluetoothDeviceList();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get device's bluetooth adapter (might be null if non present)
-        BluetoothAdapter bta = BluetoothAdapter.getDefaultAdapter();
-        ListView deviceListView = (ListView) findViewById(R.id.deviceListView);
+        registerReceiver(bluetoothEnabledReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
 
-        // Get previously paired bluetooth devices (might be disabled)
-        Set<BluetoothDevice> devices = bta.getBondedDevices();
-
-        if (!devices.isEmpty()){
-            DeviceListAdapter dla = new DeviceListAdapter(new ArrayList<BluetoothDevice>(devices), this);
-            deviceListView.setAdapter(dla);
+        // Get device's bluetooth adapter (might be null if none present)
+        bta = BluetoothAdapter.getDefaultAdapter();
+        deviceListView = (ListView) findViewById(R.id.deviceListView);
+        
+        if (bta != null) {
+            if (bta.isEnabled()) {
+                setupBluetoothDeviceList();
+            } else {
+                // If Bluetooth not enabled, request that the user enable it
+                startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), REQUEST_BLUETOOTH);
+            }
         }
 
-        deviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, ((BluetoothDevice) parent.getItemAtPosition(position)).getAddress(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupBluetoothDeviceList() {
+        // Get previously paired bluetooth devices (might be disabled)
+        if (bta != null) {
+            Set<BluetoothDevice> devices = bta.getBondedDevices();
+
+            if (!devices.isEmpty()) {
+                DeviceListAdapter dla = new DeviceListAdapter(new ArrayList<BluetoothDevice>(devices), this);
+                deviceListView.setAdapter(dla);
             }
-        });
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+        // If Bluetooth Enable request accepted
+        if (requestCode == REQUEST_BLUETOOTH && resultCode == RESULT_OK) {
+            setupBluetoothDeviceList();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(bluetoothEnabledReceiver);
     }
 }
